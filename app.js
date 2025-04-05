992047655ffa5333af901404253035cf08728bc0
shahana sherin <senurashik@gmail.com>
2025-04-05 10:19:04 +0530

    project initialize

---
+++ app.js
@@ -0,0 +1,18 @@
+const express =require("express");
+const app =express();
+const env=require("dotenv").config();
+const db =require("./config/db");
+db();
+
+
+
+
+
+const PORT=3000 || process.env.PORT;
+app.listen(PORT,()=>{
+    console.log("server running");
+})
+
+
+
+module.exports=app;
